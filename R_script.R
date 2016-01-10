281d3b68dd5063d1b3d6cff9500016e001f41327
reyfernando <reyfernando@gmail.com>
2016-01-10 17:58:58 +0100

    Update R_script.R

--- R_script.R
+++ R_script.R
@@ -1,11 +1,4 @@
 
-
-##############
-##
-# Rey Verboonen
-# Research 
-# Week 1
-
 #This assignment makes use of data from a personal activity monitoring device. This device collects data at 5 minute intervals through out the day. The data consists of two months of data from an anonymous individual collected during the months of October and November, 2012 and include the number of steps taken in 5 minute intervals each day.
 #The data for this assignment can be downloaded from the course web site:
   
